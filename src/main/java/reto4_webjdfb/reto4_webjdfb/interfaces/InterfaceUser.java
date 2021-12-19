0facf60d408312ba50589faff459febaddc2fcdc
unknown <jorge.fonseca.mt@correo.usa.edu.co>
2021-12-19 10:45:22 -0500

    first commit

---
+++ src/main/java/reto4_webjdfb/reto4_webjdfb/interfaces/InterfaceUser.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto4_webjdfb.reto4_webjdfb.interfaces;
+
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import reto4_webjdfb.reto4_webjdfb.modelo.User;
+
+/**
+ *
+ * @author JorgeFonseca
+ */
+public interface InterfaceUser extends MongoRepository<User,Integer> {
+    
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+    Optional<User> findTopByOrderByIdDesc();
+    
+}
